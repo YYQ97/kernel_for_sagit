fa4cd2a39130d0fc22b37c40c77142e1acf8d642
Kyle Yan <kyan@codeaurora.org>
2017-08-11 14:51:41 -0700

    PM / wakeup: Add msm_show_resume_irq_mask to print off wakeup irq
    Utilitize the msm_show_resume_irq_mask to print off the actual wakeup irq
    in order to help debug.
    
    Change-Id: I0e9e4225d1274bb20e1ed682059432a8161a5a71
    Signed-off-by: Kyle Yan <kyan@codeaurora.org>
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -17,6 +17,9 @@
 #include <linux/types.h>
 #include <linux/wakeup_reason.h>
 #include <trace/events/power.h>
+#include <linux/irq.h>
+#include <linux/interrupt.h>
+#include <linux/irqdesc.h>
 
 #ifdef CONFIG_DEBUG_FS
 #include <linux/debugfs.h>
@@ -1004,7 +1007,21 @@ void pm_wakeup_clear(bool reset)
 
 void pm_system_irq_wakeup(unsigned int irq_number)
 {
+	struct irq_desc *desc;
+	const char *name = "null";
+
 	if (pm_wakeup_irq == 0) {
+		if (msm_show_resume_irq_mask) {
+			desc = irq_to_desc(irq_number);
+			if (desc == NULL)
+				name = "stray irq";
+			else if (desc->action && desc->action->name)
+				name = desc->action->name;
+
+			pr_warn("%s: %d triggered %s\n", __func__,
+					irq_number, name);
+
+		}
 		pm_wakeup_irq = irq_number;
 		pm_system_wakeup();
 	}
