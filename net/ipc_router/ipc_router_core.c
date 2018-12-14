ef95cdbc7cdbc7b2165d812189705579253d4761
Yaroslav Furman <yaro330@gmail.com>
2018-12-14 14:48:31 +0300

    net: ipc_router: fix memleak with IPC_LOGGING disabled
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- net/ipc_router/ipc_router_core.c
+++ net/ipc_router/ipc_router_core.c
@@ -3966,6 +3966,7 @@ static void debugfs_init(void) {}
  */
 static void *ipc_router_create_log_ctx(char *name)
 {
+#ifdef CONFIG_IPC_LOGGING
 	struct ipc_rtr_log_ctx *sub_log_ctx;
 
 	sub_log_ctx = kmalloc(sizeof(struct ipc_rtr_log_ctx),
@@ -3987,6 +3988,9 @@ static void *ipc_router_create_log_ctx(char *name)
 	INIT_LIST_HEAD(&sub_log_ctx->list);
 	list_add_tail(&sub_log_ctx->list, &log_ctx_list);
 	return sub_log_ctx->log_ctx;
+#else
+	return NULL;
+#endif
 }
 #else
 static void *ipc_router_create_log_ctx(char *name)
