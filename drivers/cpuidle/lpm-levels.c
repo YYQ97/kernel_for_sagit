102f1dc7ff12036d77f0ecfa219e3d34a41e50d4
Sultan Alsawaf <sultan@kerneltoast.com>
2020-11-30 19:13:14 -0800

    cpuidle: lpm-levels: Fix panics when CONFIG_MSM_IDLE_STATS=n
    The cluster->stats is garbage when CONFIG_MSM_IDLE_STATS=n, in which
    case it shouldn't be dereferenced.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -692,7 +692,8 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 	if (cluster_configure(cluster, i, from_idle))
 		goto failed;
 
-	cluster->stats->sleep_time = start_time;
+	if (IS_ENABLED(CONFIG_MSM_IDLE_STATS))
+		cluster->stats->sleep_time = start_time;
 	cluster_prepare(cluster->parent, &cluster->num_children_in_sync, i,
 			from_idle, start_time);
 
@@ -700,7 +701,8 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 	return;
 failed:
 	spin_unlock(&cluster->sync_lock);
-	cluster->stats->sleep_time = 0;
+	if (IS_ENABLED(CONFIG_MSM_IDLE_STATS))
+		cluster->stats->sleep_time = 0;
 	return;
 }
 
@@ -736,7 +738,7 @@ static void cluster_unprepare(struct lpm_cluster *cluster,
 	if (!first_cpu || cluster->last_level == cluster->default_level)
 		goto unlock_return;
 
-	if (cluster->stats->sleep_time)
+	if (IS_ENABLED(CONFIG_MSM_IDLE_STATS) && cluster->stats->sleep_time)
 		cluster->stats->sleep_time = end_time -
 			cluster->stats->sleep_time;
 	lpm_stats_cluster_exit(cluster->stats, cluster->last_level, true);
