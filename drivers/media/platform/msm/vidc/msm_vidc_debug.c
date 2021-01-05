cb44bdd74336f2aa441e6b8aae8e07d5699e5546
Kazuki Hashimoto <kaz205@tuta.io>
2021-01-05 13:11:33 +0900

    treewide: Silence overly verbose logging
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

--- drivers/media/platform/msm/vidc/msm_vidc_debug.c
+++ drivers/media/platform/msm/vidc/msm_vidc_debug.c
@@ -185,7 +185,7 @@ struct dentry *msm_vidc_debugfs_init_drv(void)
 	struct dentry *f = debugfs_create_##__type(__name, S_IRUGO | S_IWUSR, \
 		dir, __value);                                                \
 	if (IS_ERR_OR_NULL(f)) {                                              \
-		dprintk(VIDC_ERR, "Failed creating debugfs file '%pd/%s'\n",  \
+		dprintk(VIDC_DBG, "Failed creating debugfs file '%pd/%s'\n",  \
 			dir, __name);                                         \
 		f = NULL;                                                     \
 	}                                                                     \
