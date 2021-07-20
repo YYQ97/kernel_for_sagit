a7597bbe8504e63a493e716858662d588fb7bb8c
yyq97 <332235240@qq.com>
2021-07-20 15:56:44 +0800

    sched/features: Disable STUNE_BOOST_BIAS_BIG

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -127,4 +127,4 @@ SCHED_FEAT(SCHEDTUNE_BOOST_UTIL, false)
  * Bias schedtune boosted tasks to higher capacity CPUs.
  * If disabled, no bias will be applied.
  */
-SCHED_FEAT(STUNE_BOOST_BIAS_BIG, true)
+SCHED_FEAT(STUNE_BOOST_BIAS_BIG, false)
