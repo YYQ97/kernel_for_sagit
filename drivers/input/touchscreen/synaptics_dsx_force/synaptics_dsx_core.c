3fa1720d8d066551d2f933eca58a85c2e8f179e3
yyq97 <332235240@qq.com>
2021-07-20 09:56:24 +0800

    driver/synaptics_dsx_force: diable log

--- drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_core.c
+++ drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_core.c
@@ -5924,8 +5924,8 @@ static int synaptics_rmi4_pm_suspend(struct device *dev)
 	if (device_may_wakeup(dev) &&
 			rmi4_data->wakeup_en &&
 			!bdata->cut_off_power) {
-		dev_info(rmi4_data->pdev->dev.parent,
-			"Enable touch irq wake\n");
+//		dev_info(rmi4_data->pdev->dev.parent,
+//			"Enable touch irq wake\n");
 		disable_irq(rmi4_data->irq);
 		enable_irq_wake(rmi4_data->irq);
 	}
@@ -5943,8 +5943,8 @@ static int synaptics_rmi4_pm_resume(struct device *dev)
 	if (device_may_wakeup(dev) &&
 			rmi4_data->wakeup_en &&
 			!bdata->cut_off_power) {
-		dev_info(rmi4_data->pdev->dev.parent,
-			"Disable touch irq wake\n");
+//		dev_info(rmi4_data->pdev->dev.parent,
+//			"Disable touch irq wake\n");
 		disable_irq_wake(rmi4_data->irq);
 		enable_irq(rmi4_data->irq);
 	}
