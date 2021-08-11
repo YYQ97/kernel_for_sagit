75235c88b84750066d3d6bbee8f07e1e2efb4c64
Sultan Alsawaf <sultan@kerneltoast.com>
2021-08-10 20:43:56 -0700

    sched/core: Always panic when scheduling in atomic context
    Scheduling in atomic context is indicative of a serious problem that,
    although may not be immediately lethal, can lead to strange issues and
    eventually a panic. We should therefore panic the first time it's
    detected.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3211,8 +3211,7 @@ static noinline void __schedule_bug(struct task_struct *prev)
 #ifdef CONFIG_PANIC_ON_SCHED_BUG
 	BUG();
 #endif
-	dump_stack();
-	add_taint(TAINT_WARN, LOCKDEP_STILL_OK);
+	panic("scheduling while atomic\n");
 }
 
 /*
