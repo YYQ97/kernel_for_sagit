96064098bfcadcf8b342d02cd11fb7577c2a3147
yyq97 <332235240@qq.com>
2021-09-03 01:18:05 +0800

    dsp: q6voice: Check size of payload before access

--- sound/soc/msm/qdsp6v2/q6voice.c
+++ sound/soc/msm/qdsp6v2/q6voice.c
@@ -6683,7 +6683,7 @@ static int32_t qdsp_cvs_callback(struct apr_client_data *data, void *priv)
 	}
 
 	if (data->opcode == APR_BASIC_RSP_RESULT) {
-		if (data->payload_size) {
+		if (data->payload_size >= (2 * sizeof(uint32_t))) {
 			ptr = data->payload;
 
 			pr_debug("%x %x\n", ptr[0], ptr[1]);
