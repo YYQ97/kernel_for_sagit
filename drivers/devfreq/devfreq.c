e044a1f1ddbc3f2dad47d5f6d733001356340ed5
yyq97 <332235240@qq.com>
2021-10-05 17:14:33 +0800

    Revert "PM / devfreq: Fix race condition between suspend/resume and governor_store"
    This reverts commit 944fedcf2d1681676beae9dfc9050004549cf4ee.

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -435,7 +435,7 @@ static void _remove_devfreq(struct devfreq *devfreq)
 		devfreq->profile->exit(devfreq->dev.parent);
 
 	mutex_destroy(&devfreq->lock);
-	mutex_destroy(&devfreq->event_lock);
+	mutex_destroy(&devfreq->sysfs_lock);
 	kfree(devfreq);
 }
 
@@ -492,7 +492,7 @@ struct devfreq *devfreq_add_device(struct device *dev,
 	}
 
 	mutex_init(&devfreq->lock);
-	mutex_init(&devfreq->event_lock);
+	mutex_init(&devfreq->sysfs_lock);
 	mutex_lock(&devfreq->lock);
 	devfreq->dev.parent = dev;
 	devfreq->dev.class = devfreq_class;
@@ -641,19 +641,14 @@ EXPORT_SYMBOL(devm_devfreq_remove_device);
  */
 int devfreq_suspend_device(struct devfreq *devfreq)
 {
-	int ret;
-
 	if (!devfreq)
 		return -EINVAL;
 
 	if (!devfreq->governor)
 		return 0;
 
-	mutex_lock(&devfreq->event_lock);
-	ret = devfreq->governor->event_handler(devfreq,
+	return devfreq->governor->event_handler(devfreq,
 				DEVFREQ_GOV_SUSPEND, NULL);
-	mutex_unlock(&devfreq->event_lock);
-	return ret;
 }
 EXPORT_SYMBOL(devfreq_suspend_device);
 
@@ -667,18 +662,14 @@ EXPORT_SYMBOL(devfreq_suspend_device);
  */
 int devfreq_resume_device(struct devfreq *devfreq)
 {
-	int ret;
 	if (!devfreq)
 		return -EINVAL;
 
 	if (!devfreq->governor)
 		return 0;
 
-	mutex_lock(&devfreq->event_lock);
-	ret = devfreq->governor->event_handler(devfreq,
+	return devfreq->governor->event_handler(devfreq,
 				DEVFREQ_GOV_RESUME, NULL);
-	mutex_unlock(&devfreq->event_lock);
-	return ret;
 }
 EXPORT_SYMBOL(devfreq_resume_device);
 
@@ -834,7 +825,7 @@ static ssize_t governor_store(struct device *dev, struct device_attribute *attr,
 		goto out;
 	}
 
-	mutex_lock(&df->event_lock);
+	mutex_lock(&df->sysfs_lock);
 	if (df->governor) {
 		ret = df->governor->event_handler(df, DEVFREQ_GOV_STOP, NULL);
 		if (ret) {
@@ -860,7 +851,7 @@ static ssize_t governor_store(struct device *dev, struct device_attribute *attr,
 	}
 
 gov_stop_out:
-	mutex_unlock(&df->event_lock);
+	mutex_unlock(&df->sysfs_lock);
 out:
 	mutex_unlock(&devfreq_list_lock);
 
@@ -935,10 +926,10 @@ static ssize_t polling_interval_store(struct device *dev,
 	if (ret != 1)
 		return -EINVAL;
 
-	mutex_lock(&df->event_lock);
+	mutex_lock(&df->sysfs_lock);
 	df->governor->event_handler(df, DEVFREQ_GOV_INTERVAL, &value);
 	ret = count;
-	mutex_unlock(&df->event_lock);
+	mutex_unlock(&df->sysfs_lock);
 
 	return ret;
 }
@@ -956,7 +947,7 @@ static ssize_t min_freq_store(struct device *dev, struct device_attribute *attr,
 	if (ret != 1)
 		return -EINVAL;
 
-	mutex_lock(&df->event_lock);
+	mutex_lock(&df->sysfs_lock);
 	mutex_lock(&df->lock);
 	max = df->max_freq;
 	if (value && max && value > max) {
@@ -969,7 +960,7 @@ static ssize_t min_freq_store(struct device *dev, struct device_attribute *attr,
 	ret = count;
 unlock:
 	mutex_unlock(&df->lock);
-	mutex_unlock(&df->event_lock);
+	mutex_unlock(&df->sysfs_lock);
 	return ret;
 }
 
@@ -991,7 +982,7 @@ static ssize_t max_freq_store(struct device *dev, struct device_attribute *attr,
 	if (ret != 1)
 		return -EINVAL;
 
-	mutex_lock(&df->event_lock);
+	mutex_lock(&df->sysfs_lock);
 	mutex_lock(&df->lock);
 	min = df->min_freq;
 	if (value && min && value < min) {
@@ -1004,7 +995,7 @@ static ssize_t max_freq_store(struct device *dev, struct device_attribute *attr,
 	ret = count;
 unlock:
 	mutex_unlock(&df->lock);
-	mutex_unlock(&df->event_lock);
+	mutex_unlock(&df->sysfs_lock);
 	return ret;
 }
 static DEVICE_ATTR_RW(min_freq);
