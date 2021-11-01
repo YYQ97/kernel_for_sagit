3473df7eb537aae0941688d067db97fa9dc088c9
yyq97 <332235240@qq.com>
2021-11-01 22:19:05 +0800

    Revert "Net:Tcp.c: Replace TCP_NLA_PDA with NULL"
    This reverts commit c94c1996e3c31d5ea6401734062017dc3972e881.

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2846,11 +2846,11 @@ struct sk_buff *tcp_get_timestamping_opt_stats(const struct sock *sk)
 
 	tcp_get_info_chrono_stats(tp, &info);
 	nla_put(stats, TCP_NLA_BUSY,
-			  info.tcpi_busy_time, NULL);
+			  info.tcpi_busy_time, TCP_NLA_PAD);
 	nla_put(stats, TCP_NLA_RWND_LIMITED,
-			  info.tcpi_rwnd_limited, NULL);
+			  info.tcpi_rwnd_limited, TCP_NLA_PAD);
 	nla_put(stats, TCP_NLA_SNDBUF_LIMITED,
-			  info.tcpi_sndbuf_limited, NULL);
+			  info.tcpi_sndbuf_limited, TCP_NLA_PAD);
 	return stats;
 }
 
